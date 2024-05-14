9f4c1cb5299e143f0ed6571e1e2a1b6d8eb5564d
Clayton1987 <151626544+Clayton1987@users.noreply.github.com>
2024-05-14 10:30:02 -0300

    UPDATE
    UPDATE

---
+++ JAVA_SCRIPT_TST/TST_CODIGOS/MEU_JS.js
@@ -0,0 +1,14 @@
+const msg_tela = 'uma coisa';
+let msg_tela02 = 'segunda coisa';
+
+console.log('Qualquer coisa');
+console.log(msg_tela);
+//msg_tela = 'outra coisa';
+console.log(msg_tela02);
+msg_tela02 = 'outra coisa';
+console.log(msg_tela02);
+
+//const -> não pode ser alterado
+//let -> pode ser alterado
+//var -> pode ser alterado
+
---
+++ JAVA_SCRIPT_TST/TST_CODIGOS/resolucao.js
@@ -0,0 +1,27 @@
+console.log('Resultando do Exercicio');
+const primeiraVariavel = 2148;
+let segundaVariavel;
+
+console.log(`O valor da primeira variavel é : ${primeiraVariavel}`);
+
+segundaVariavel = 23;
+
+console.log(`Minha segunda variavel é: ${segundaVariavel}`);
+console.log(`a Soma é: ${primeiraVariavel + segundaVariavel}`);
+segundaVariavel = segundaVariavel **2;
+console.log(segundaVariavel);
+segundaVariavel = segundaVariavel / 3;
+console.log(segundaVariavel);
+
+const booleana1 = true;
+const booleana2 = false;
+
+console.log(`${booleana1 && booleana2}`);
+console.log(`${booleana1 || booleana2}`);
+
+const booleana3 = false;
+console.log(`${booleana1 && booleana2 && booleana3}`);
+console.log(`${booleana1 || booleana2 || booleana3 }`);
+const traco = 'tst'
+const vez = 80
+console.log(traco * 80);
