75cfb1864f99acb6b7e4ed5b2b8239dd08da662e
Clayton1987 <151626544+Clayton1987@users.noreply.github.com>
2024-09-29 08:58:38 -0300

    update

--- JAVA_SCRIPT_TST/TST_CODIGOS/resolucao.js
+++ JAVA_SCRIPT_TST/TST_CODIGOS/resolucao.js
@@ -25,3 +25,68 @@ console.log(`${booleana1 || booleana2 || booleana3 }`);
 const traco = 'tst'
 const vez = 80
 console.log(traco * 80);
+
+const arrayNumber = [1, 2, 3, 4, 5];
+
+console.log(arrayNumber);
+
+const Pessoa = {
+    nome:"Fulano",
+    idade: 30,
+    profissao: "Programador",
+    endereco: "Rua 1, 123",
+    telefone: "1234567890",
+};
+
+function pergarComanda(){
+    console.log("Ola Boa noite");
+    console.log("Pegue sua comanda aqui");
+    console.log("Bom apetite!!!");
+    console.log("-----------------------------------");
+    }
+
+console.log(Pessoa);
+console.log(Pessoa.profissao);
+console.log(Pessoa['endereco']);
+
+pergarComanda();
+
+function calcularQuadrado(numeroDaVez) {
+    const lado = 5;
+    const area = lado ** lado;
+    console.log(`O numero da vez é o :${numeroDaVez}`)
+    console.log(`O numero da vez (${numeroDaVez}) ao Quadrado vale ${numeroDaVez **2}`);
+    console.log('-----------------------------------------------------');
+}
+
+calcularQuadrado(1);
+calcularQuadrado(2);
+calcularQuadrado(3);
+calcularQuadrado(4);
+
+let casa = {
+    cor: "Azul",
+    sala: 15,
+    cozinha: 10,
+    banheiro: 8,
+    quarto: 20,
+    calcularTamanhoTotalCasaM2: function() { 
+        const tamanhoTotal = casa.sala + casa.cozinha + casa.banheiro + casa.quarto;
+        console.log(`O tamanho da casa é de ${tamanhoTotal}`);
+        return tamanhoTotal;
+    },
+}
+
+casa.calcularTamanhoTotalCasaM2()
+
+
+function calcularSquareRoot(radicand){
+    return radicand ** (1 / 2);
+}
+
+function calculatePythagoras(side1, side2){
+    const sum = side1 ** 2 + side2 ** 2;
+    return calcularSquareRoot(sum);
+}
+
+console.log(`A hipotenusa de um triangulo de lados 3 e 4 é: ${calculatePythagoras(3, 4)}`)
